c98a1e16bc01ed014f6040827779f739f004dcf2
Elena <37418633+yellowbirdwithme@users.noreply.github.com>
2018-10-12 14:02:48 +0300

    New features
    Method tokenize of Tokenizer is redefined with the help of method generate.
    
    Methods added for class Tokenizer:
    _getType: identifies a character as belonging to one of following types:
    a - alphabetic
    d - digit
    s - space
    p - punctuation
    o - other
    generate_with_type, tokenize_with_type: tokenize a string into TypeToken instances.
    
    New classes added:
    TypeToken: Token with type attribute tp (str) that takes one of the values listed above.
    
    Now the module can tokenize the whole string into TypeToken instances.

--- lenin_tester.py
+++ lenin_tester.py
@@ -2,6 +2,124 @@ import unittest
 from collections import Generator
 from lenin_tokenizer import Tokenizer
 
+class GetTypeTest(unittest.TestCase):
+    def setUp(self):
+        self.tokenizer = Tokenizer()
+    def test_alpa_latina(self):
+        result = self.tokenizer._getType('c')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "a")
+        result = self.tokenizer._getType('Z')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "a")
+    def test_alpa_russa(self):
+        result = self.tokenizer._getType('а')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "a")
+        result = self.tokenizer._getType('Ё')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "a")
+        result = self.tokenizer._getType('ё')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "a")
+        result = self.tokenizer._getType('Я')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "a")
+        result = self.tokenizer._getType('А')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "a")
+    def test_digit(self):
+        result = self.tokenizer._getType('0')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "d")
+        result = self.tokenizer._getType('1')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "d")
+        result = self.tokenizer._getType('3')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "d")
+        result = self.tokenizer._getType('9')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "d")
+    def test_space(self):
+        result = self.tokenizer._getType(' ')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "s")
+    def test_punct(self):
+        result = self.tokenizer._getType('.')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "p")
+        result = self.tokenizer._getType(',')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "p")
+        result = self.tokenizer._getType('!')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "p")
+        result = self.tokenizer._getType('¿')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "p")
+        result = self.tokenizer._getType('/')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "p")
+        result = self.tokenizer._getType(')')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, "p")
+
+class GenerateWithTypeTest(unittest.TestCase):
+    def setUp(self):
+        self.tokenizer = Tokenizer()
+
+    def test_output_type(self):
+        result = self.tokenizer.generate_with_type('test')
+        self.assertIsInstance(result, Generator)
+
+    def test_alpha_and_spaces_only(self):
+        result = list(self.tokenizer.generate_with_type('I am very tired'))
+        self.assertEqual(len(result), 7)
+        self.assertEqual(result[0].s, 'I')
+        self.assertEqual(result[0].pos, 0)
+        self.assertEqual(result[0].tp, 'a')
+        self.assertEqual(result[1].s, ' ')
+        self.assertEqual(result[1].pos, 1)
+        self.assertEqual(result[1].tp, 's')
+        self.assertEqual(result[6].s, 'tired')
+        self.assertEqual(result[6].pos, 10)
+        self.assertEqual(result[6].tp, 'a')
+    def test_all_types_of_character(self):
+        result = list(self.tokenizer.generate_with_type(
+            'I am very  tired, I want to go to sleep at 6:30!!!'))
+        self.assertEqual(len(result), 27)
+        self.assertEqual(result[0].s, 'I')
+        self.assertEqual(result[0].pos, 0)
+        self.assertEqual(result[0].tp, 'a')
+        self.assertEqual(result[5].s, '  ')
+        self.assertEqual(result[5].pos, 9)
+        self.assertEqual(result[5].tp, 's')
+        self.assertEqual(result[7].s, ',')
+        self.assertEqual(result[7].pos, 16)
+        self.assertEqual(result[7].tp, 'p')
+        self.assertEqual(result[23].s, '6')
+        self.assertEqual(result[23].pos, 43)
+        self.assertEqual(result[23].tp, 'd')
+        self.assertEqual(result[25].s, '30')
+        self.assertEqual(result[25].pos, 45)
+        self.assertEqual(result[25].tp, 'd')
+        self.assertEqual(result[26].s, '!!!')
+        self.assertEqual(result[26].pos, 47)
+        self.assertEqual(result[26].tp, 'p')
+
+    def test_empty_string(self):
+        result = list(self.tokenizer.generate(""))
+        self.assertEqual(len(result), 0)
+
+    def test_error_wrong_input_number(self):
+        with self.assertRaises(ValueError):
+            list(self.tokenizer.generate(42))
+            
+    def test_error_wrong_input_list(self):
+        with self.assertRaises(ValueError):
+            list(self.tokenizer.generate(['this', 'is', 'a', 'trap']))
+
 class GenerateTest(unittest.TestCase):
     def setUp(self):
         self.tokenizer = Tokenizer()
--- lenin_tokenizer.py
+++ lenin_tokenizer.py
@@ -1,6 +1,8 @@
 """
 This module allows to tokenize a string of characters.
 """
+from unicodedata import category
+
 class Token(object):
     """
     Token is a word that consists solely of alphabetic characters
@@ -23,13 +25,65 @@ class Token(object):
     def __repr__(self):
         return self.s + " " + str(self.pos)
 
+class TypeToken(Token):
+    """
+    TypeToken is Token with a type.
+    
+    Attributes:
+        pos (int): position of the first character of the token.
+        s (str): string represention of the token.
+        tp (str): type of token. Takes one of following values:
+            a - alphabetic
+            d - digit
+            s - space
+            p - punctuation
+            o - other
+    """
+    def __init__(self, pos, s, tp):
+        """
+        Initialises itself.
+
+        Args:
+            pos (int): position of the first character of the token.
+            s (str): string represention of the token.
+            tp (str): type of token
+        """
+        self.pos = pos
+        self.s = s
+        self.tp = tp
 
 class Tokenizer(object):
     """
     Class tokenizer that can tokenize a string using method tokenize
     or generate.
     """
-    
+    def _getType(self, c):
+        """
+        Gets type of a character.
+
+        Args:
+            c (str): character to identify the type of.
+
+        Returns:
+            type of the character as string:
+                a - alphabetic
+                d - digit
+                s - space
+                p - punctuation
+                o - other
+        """
+        cat = category(c)
+        if cat[0]=="L":
+            return "a"
+        elif cat[0]=="N":
+            return "d"
+        elif cat[0]=="Z":
+            return "s"
+        elif cat[0]=="P":
+            return "p"
+        else:
+            return "o"
+        
     def generate(self, text):
         """
         Generator.
@@ -43,9 +97,9 @@ class Tokenizer(object):
             Token instances.
 
         Raises:
-            ValueError
+            ValueError: in case text is not str
         """
-        if not type(text) is str:
+        if not isinstance(text, str):
             raise ValueError
         
         # check the first symbol of a sequence to set initial value of pos
@@ -83,41 +137,50 @@ class Tokenizer(object):
             List of Token instances.
             
         Raises:
-            ValueError    
+            ValueError: in case text is not str    
         """
-        if not type(text) is str:
+        return list(self.generate(text))
+
+    def generate_with_type(self,text):
+        """
+        Generator.
+        Divides a string into TypeToken instances
+        
+        Args:
+            text (str): String to be tokenized.
+        
+        Yields:
+            TypeToken instances.
+
+        Raises:
+            ValueError: in case text is not str
+        """
+        if not isinstance(text, str):
             raise ValueError
+        
         if not text:
-            return []
-        tokens = []
-        # check the first symbol of a sequence to set initial value of pos
-        # pos>=0 is the start position of a word
-        # pos=-1 means we are between the words waiting for a new word to start
-        if text[0].isalpha():
-            pos = 0
-        else:
-            pos = -1
-            
+            return
+        
+        previousType = ""
+        pos = 0
         for i, c in enumerate(text):
-            # when the word ends add it to the list, set pos to -1
-            if not c.isalpha() and pos>=0:
-                tokens.append(Token(pos, text[pos:i]))
-                pos = -1
-            # finds the beginning of the next word
-            if  not text[i-1].isalpha() and c.isalpha():
-                pos=i
-        # if the last character was alphabetic, we did not add
-        # the last token to the list
-        if c.isalpha():
-            tokens.append(Token(pos, text[pos:i+1]))
-        return tokens
+            currentType = self._getType(c)
+            if currentType != previousType and i>0:
+                yield TypeToken(pos, text[pos:i], previousType)
+                pos = i
+            previousType = currentType
+        yield TypeToken(pos, text[pos:i+1], previousType)
+
+    def tokenize_with_type(self, text):
+        return list(generate_with_type(text))
+
 
 def main():
-    text = ''
+    text = 'I am very  tired, I want to go to sleep at 6:30!'
     tok = Tokenizer()
-    tokens = tok.tokenize(text)
+    tokens = list(tok.generate_with_type(text))
     for token in tokens:
-        print(token.s, token.pos)
+        print(token.pos, token.tp, token.s)
 
 
 if __name__ == "__main__":
